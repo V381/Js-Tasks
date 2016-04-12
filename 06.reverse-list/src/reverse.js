0b380b178be539445d8795b80bd0cb190fd94870
V381 <paavlee@yahoo.com>
2016-04-12 15:13:48 +0200

    initial

---
+++ 06.reverse-list/src/reverse.js
@@ -0,0 +1,14 @@
+/**
+ * Write function that reverses a list, preferably in place.
+ */
+var List = function() {
+  'use strict';
+
+  function reverseList(list) {
+
+  }
+
+  return {
+    reverseList: reverseList
+  };
+};
