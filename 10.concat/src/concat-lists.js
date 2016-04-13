b13a66b6a62d2fea41577c079368edb2a9a9439d
Pavle <V381@users.noreply.github.com>
2016-04-13 10:23:08 +0200

    Update concat-lists.js

--- 10.concat/src/concat-lists.js
+++ 10.concat/src/concat-lists.js
@@ -5,7 +5,10 @@ var List = function() {
   'use strict';
 
   function concatenate(list1, list2) {
-
+  		list2.forEach(function(val) {
+  			list1.push(val);
+  		});
+  		return list1;
   }
 
   return {
