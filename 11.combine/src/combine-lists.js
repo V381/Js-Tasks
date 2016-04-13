b614f79aa39139456c0bbf703b5776bc4ef373f3
Pavle <V381@users.noreply.github.com>
2016-04-13 10:23:32 +0200

    Update combine-lists.js

--- 11.combine/src/combine-lists.js
+++ 11.combine/src/combine-lists.js
@@ -5,7 +5,13 @@ var List = function() {
   'use strict';
 
   function combine(list1, list2) {
-
+  		var c = list1.map(function (e, i) {
+  		if(list2[i] === undefined){
+  			return [list1[i]];
+  		}
+    		return [list1[i], list2[i]];
+		});
+		return [].concat.apply([], c)
   }
 
   return {
