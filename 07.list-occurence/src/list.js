4263ae6c6316bb15edc8920601d221259fabe4b2
Pavle <V381@users.noreply.github.com>
2016-04-13 10:21:41 +0200

    Update list.js

--- 07.list-occurence/src/list.js
+++ 07.list-occurence/src/list.js
@@ -5,7 +5,27 @@ var List = function() {
   'use strict';
 
   function has(list, element) {
+  	 var value, fortyFour;
+  	 list.forEach(function(val, i, arr) {
+  	 	if(val === element){
+  	 		value = true;
+  	 	}
 
+  	 	if(Array.isArray(val)){
+  	 		fortyFour = false;
+  	 	}
+
+  	 	if(val != element){
+  	 		fortyFour = false;
+  	 	}
+
+  	 });
+
+  	 if(value){
+  	 	return value;
+  	 }else{
+  	 	return fortyFour;
+  	 }
   }
 
   return {
