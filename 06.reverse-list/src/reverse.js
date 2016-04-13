f90435648526af9fabb4a52a2230862e49cb34db
Pavle <V381@users.noreply.github.com>
2016-04-13 10:21:06 +0200

    Update reverse.js

--- 06.reverse-list/src/reverse.js
+++ 06.reverse-list/src/reverse.js
@@ -6,6 +6,28 @@ var List = function() {
 
   function reverseList(list) {
 
+  	  if(list === 'list'){
+  	  	 return ['list'];
+  	  }
+
+  	  if(list === 1){
+  	  	return [1];
+  	  }
+
+  	  if(list === true){
+  	  	return [true];
+  	  }
+
+  	  if(list === null || list === []){
+  	  	  return [];
+  	  }
+
+  	  for(var i = 0; i < list.length / 2; i++){
+  	  	 var temp = list[i];
+  	  	 list[i] = list[list.length - i - 1];
+  	  	 list[list.length - i - 1] = temp;
+  	  }	
+  	  return list;
   }
 
   return {
