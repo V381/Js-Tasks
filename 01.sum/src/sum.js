35d708b878b01648a9284d890f1af0c4158cf733
Pavle <V381@users.noreply.github.com>
2016-04-13 10:18:56 +0200

    Update sum.js

--- 01.sum/src/sum.js
+++ 01.sum/src/sum.js
@@ -5,6 +5,27 @@ var Sum = function() {
   'use strict';
 
   function sumNumbers(n) {
+  	
+  	var res = 0, nRes;
+
+  	if(n < 0){
+  		n = Math.abs(n);
+  		nRes = true;
+  	}
+
+  	if(n === 0){
+  		return 0;
+  	}
+
+  	for (var i = 0; i <= n; i++) {
+  		res += i;
+  	}
+
+  	if(nRes){
+  		return -Math.abs(res);
+  	}
+
+  	return res;		
 
   }
 
