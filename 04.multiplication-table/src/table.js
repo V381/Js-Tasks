17db2a63c6ed858a0500ae0696252484da7acb0b
Pavle <V381@users.noreply.github.com>
2016-04-13 22:10:28 +0200

    Update table.js

--- 04.multiplication-table/src/table.js
+++ 04.multiplication-table/src/table.js
@@ -6,7 +6,26 @@ var MultiplicationTable = function() {
   'use strict';
 
   function generate5() {
+  	 // print main table
 
+     var arr = [[], [], [], [], [], []];
+
+    (function() {
+      for(var i = 0; i <= 5; i++){
+        arr[0].push(i)
+      }
+    })();
+
+    for(var z = 1; z <= 5; z++){
+      arr[z].push(z);
+    }
+
+    for (var j=1; j<=5; j++) {
+      for (var i=1; i<=5; i++) {
+        arr[i].push(i * j)
+      }
+    }
+    return arr;
   }
 
   return {
