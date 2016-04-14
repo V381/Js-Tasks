e8a4cba9a8bca285dacef8c8c25f1a19ad8807b8
Pavle <V381@users.noreply.github.com>
2016-04-14 14:53:03 +0200

    Updated

--- 04.multiplication-table/src/table.js
+++ 04.multiplication-table/src/table.js
@@ -2,28 +2,32 @@
  * Write a function that outputs a multiplication table for numbers up to 5.
  * The table should be a two-dimensional array.
  */
-var MultiplicationTable = function() {
-  'use strict';
+var MultiplicationTable = function () {
+    'use strict';
 
-  function generate5(n) {
-  	var arr = [[], [], [], [], [], []];
-    (function() {
-      for(var i = 0; i <= 5; i++){
-        arr[0].push(i)
-      }
-    })();
-    for(var z = 1; z <= 5; z++){
-      arr[z].push(z);
-    }
-    for (var j=1; j<=5; j++) {
-      for (var i=1; i<=5; i++) {
-        arr[i].push(i * j)
-      }
-    }
-    return arr;
-  }
+    function generate5(n) {
+        var arr = [[], [], [], [], [], []];
 
-  return {
-    generate5: generate5
-  };
+        (function () {
+            for (var i = 0; i <= 5; i++) {
+                arr[0].push(i)
+            }
+        })();
+
+        for (var z = 1; z <= 5; z++) {
+            arr[z].push(z);
+        }
+
+        for (var j = 1; j <= 5; j++) {
+            for (var i = 1; i <= 5; i++) {
+                arr[i].push(i * j)
+            }
+        }
+
+        return arr;
+    }
+
+    return {
+        generate5: generate5
+    };
 };
